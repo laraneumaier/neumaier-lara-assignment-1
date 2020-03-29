be7bb678323c41c6868b25d601fe9201a57d1f8d
laraneumaier <neumaier.lara@web.de>
2020-03-29 21:22:40 +0200

    Slideshow eingefüht, momentanes Problem:  Slideshow fängt nicht mit dem ersten Bild an

---
+++ javascript.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+zeigeSlides(slideIndex); 
+
+// kontrolliert vor und zurück von den button
+function plusSlides(n) {
+  zeigeSlides(slideIndex +=n);
+}
+
+// der momentante Slide ist der, der gezeigt wird und slideindex wird n zugewiesen 
+function momentanerSlide (n){
+    zeigeSlides(slideIndex = n);
+}
+
+function zeigeSlides(n){
+    let i;
+    let slides = new Array;
+    slides = document.getElementsByClassName("slides");
+    if (n > slides.length){
+        slideIndex = 1;
+    }
+    if (n < 1){
+        slideIndex = slide.length;
+    }
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+
