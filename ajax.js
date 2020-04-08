3fb341b524d179c848ff5f46fbbadf0f14e09fc8
laraneumaier <neumaier.lara@web.de>
2020-04-08 20:42:08 +0200

    ajax-Code kommentiert

--- ajax.js
+++ ajax.js
@@ -7,12 +7,13 @@ fetch('data.json')
     appendData(data);
 })
 .catch(function (err) {
-    console.log('error: ' + err);
+    console.log('error: ' + err); // Fehler abfangen
 });
+
 function appendData(data) {
     var Container = document.getElementById("personen");
     var randomnumber = (Math.floor(Math.random() * 9)); // random number von 0-9
     var div = document.createElement("div");
     div.innerHTML = data[randomnumber].firstName + " " + data[randomnumber].lastName + "</br>" + "Email: " + data[randomnumber].email;
-    Container.appendChild(div);  
+    Container.appendChild(div);  // setze div Element unter container 
 }
