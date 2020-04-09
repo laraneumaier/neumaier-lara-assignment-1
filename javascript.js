2940d3cfc2ab757bd73fa5d3c2dbaf99494828a2
laraneumaier <neumaier.lara@web.de>
2020-04-09 23:01:15 +0200

    JavaScript Dateien zusammengeführt

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", initScript());
+window.addEventListener("load", initScript);
 
 function initScript(){
     zeigeSlides();
@@ -6,6 +6,7 @@ function initScript(){
     canvasGesichtsauer();
     canvasGesichtLacheln();
     canvasGesichtNeutral();
+    ajax();
 }
 
 /* SLIDESHOW */
@@ -155,3 +156,24 @@ function fixNavigation (){
     }
 }
 
+function ajax(){
+    fetch('data.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err); // Fehler abfangen
+    });
+    
+    function appendData(data) {
+        let Container = document.getElementById("personen");
+        let randomnumber = (Math.floor(Math.random() * 9)); // random number von 0-9
+        let div = document.createElement("div");
+        div.innerHTML = data[randomnumber].firstName + " " + data[randomnumber].lastName + "</br>" + "Email: " + data[randomnumber].email;
+        Container.appendChild(div);  // heftet div Element an Container
+    }
+    
+}
