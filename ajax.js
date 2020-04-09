b75db300491d65987540f0dfe4b0d52ad02d842f
laraneumaier <neumaier.lara@web.de>
2020-04-09 22:41:01 +0200

    var durch let ersetzt

--- ajax.js
+++ ajax.js
@@ -11,9 +11,9 @@ fetch('data.json')
 });
 
 function appendData(data) {
-    var Container = document.getElementById("personen");
-    var randomnumber = (Math.floor(Math.random() * 9)); // random number von 0-9
-    var div = document.createElement("div");
+    let Container = document.getElementById("personen");
+    let randomnumber = (Math.floor(Math.random() * 9)); // random number von 0-9
+    let div = document.createElement("div");
     div.innerHTML = data[randomnumber].firstName + " " + data[randomnumber].lastName + "</br>" + "Email: " + data[randomnumber].email;
-    Container.appendChild(div);  // setze div Element unter container 
+    Container.appendChild(div);  // heftet div Element an Container
 }
